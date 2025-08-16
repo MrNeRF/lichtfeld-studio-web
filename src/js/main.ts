95855cdf2b3f9e73038d92ed0c7b5b7cd797a14a
simonbethke <simon.bethke@gmail.com>
2025-08-16 22:43:45 +0200

    Cleaning up the splat initialization

---
+++ src/js/main.ts
@@ -0,0 +1,15 @@
+import '../scss/styles.scss'
+import { SplatCanvas } from './SplatCanvas';
+
+
+window.addEventListener('DOMContentLoaded', async () => {
+    document.querySelectorAll('canvas[data-scene]').forEach((node) => {
+        const canvas = node as  HTMLCanvasElement;
+        const splatCanvas = new SplatCanvas(canvas);
+
+        canvas.addEventListener('click', () => {
+            splatCanvas.togglePose(4000);
+        });
+    });
+});
+

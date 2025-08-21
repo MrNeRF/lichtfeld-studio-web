34a9178b99f186a7fb1a4c19387941986724f478
simonbethke <simon.bethke@gmail.com>
2025-08-21 13:22:31 +0200

    disable jekyll

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   base: "/lichtfeld-studio-web",
   outDir: "./docs",
   vite: {
